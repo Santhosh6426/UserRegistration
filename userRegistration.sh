518c0c31fd5414abc1f8f886c646d2e72af701bc
Santhosh V <santhosh.v6426@gmail.com>
2020-09-15 23:00:16 +0530

    Added:Added program to take Mobile number

--- userRegistration.sh
+++ userRegistration.sh
@@ -17,3 +17,6 @@ fun $secondName
 pattern="^[a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
 read -p "Enter email : " email
 fun $email
+pattern="^(9?1?)[:space:]?[0-9]{10}$"
+read -p "Enter 10 digit mobile number : " mob
+fun $mob
