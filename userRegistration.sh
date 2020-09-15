a68bcd5f2afcd43f80d579099b6cae681823d9ed
Santhosh V <santhosh.v6426@gmail.com>
2020-09-15 22:13:34 +0530

    Added:Added program to take first name.

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,1 +1,10 @@
 echo "Welcome to User Registration program"
+#!/bin/bash
+pattern="^([A-Z]{1}[a-zA-Z]{2,})$"
+read -p "Enter First Name : " firstName
+if [[ $firstName =~ $pattern ]]
+then
+	echo "Valid"
+else
+	echo "Invalid!"
+fi
