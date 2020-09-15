39b8568b6d7ac0076cb541d9456968a9084bea1f
Santhosh V <santhosh.v6426@gmail.com>
2020-09-15 22:20:36 +0530

    Added:Added program to take second name.

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,10 +1,16 @@
 echo "Welcome to User Registration program"
 #!/bin/bash
+fun() {
+	if [[ $1 =~ $pattern ]]
+	then
+		echo "Valid"
+	else
+		echo "Invalid!"
+	fi
+}
 pattern="^([A-Z]{1}[a-zA-Z]{2,})$"
 read -p "Enter First Name : " firstName
-if [[ $firstName =~ $pattern ]]
-then
-	echo "Valid"
-else
-	echo "Invalid!"
-fi
+fun $firstName
+pattern="^([A-Z]{1}[a-zA-Z]{2,})$"
+read -p "Enter First Name : " secondName
+fun $secondName
