2cdfbfb18a0fe2f2aec3f252a2d9b588cd67e09b
Santhosh V <santhosh.v6426@gmail.com>
2020-09-15 22:31:36 +0530

    Added:Added program to take email.

--- userRegistration.sh
+++ userRegistration.sh
@@ -14,3 +14,6 @@ fun $firstName
 pattern="^([A-Z]{1}[a-zA-Z]{2,})$"
 read -p "Enter First Name : " secondName
 fun $secondName
+pattern="^[a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
+read -p "Enter email : " email
+fun $email
